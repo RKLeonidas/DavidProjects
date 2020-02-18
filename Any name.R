caedcffe26fd95a07a732e5099b433a78121681f
davidrochha <adavidrocha@outlook.com>
2020-02-18 10:58:50 -0300

    any

---
+++ Any name.R
@@ -0,0 +1,20 @@
+install.packages("dplyr")
+
+iris
+library(dplyr)
+glimpse(iris)
+select(iris,Petal.Length)
+filter(iris,Petal.Length>2)
+iris %>% 
+  filter(Petal.Length>2) %>% 
+  mutate(Petal.Length=Petal.Length+2) %>% 
+  select(Species,Petal.Width)%>% 
+  glimpse()
+iris %>%
+  transmute(Petal=Petal.Length+2,Species)
+
+iris %>% 
+  group_by(Species) %>% 
+  summarise(iris)
+git config --global user.email "adavidrocha@outlook.com"
+git config --global user.name "davidrochha"
